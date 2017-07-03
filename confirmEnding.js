a10edb42b50e1f0db236e45fd3ac80c47fd55a95
Alex Ticovschi <alexmticovschi@gmail.com>
2017-07-03 22:26:53 +0100

    Update and rename Confirm the Ending to confirmEnding.js

--- confirmEnding.js
+++ confirmEnding.js
@@ -1,19 +1,16 @@
 
 function confirmEnding(str, target) {
-  
+ 
 //   // create a variable and store sthe starting position (Location at which to begin extracting characters)
 //   // substr() synatax :  "str.substr(start [, length])"
 //   var pos = str.length - target.length;
   
 //   // if the extracted characters are equal to the target return true, else return false
-//   if(str.substr(pos, str.length) === target) {
-//     return true;
-//   } else {
-//     return false;
-//   }
+//   return (str.substr(pos, str.length) === target) ? true : false;
   
    // check if the last character of the string is the same as the target 
    return str.substr(-target.length) === target;
 }
 
 confirmEnding("Bastian", "n");
+
