dde13cc38977fe5d046898e96042cb4d98ea9215
Alex Ticovschi <alexmticovschi@gmail.com>
2017-07-05 20:26:29 +0100

    Update repeatString.js

--- repeatString.js
+++ repeatString.js
@@ -1,10 +1,10 @@
 function repeatStringNumTimes(str, num) {
-
-//   if(num > 0) {
-//      return str.repeat(num);
-//   } else {
-//     return str.repeat(0);
-//   }
+    
+//     if(num > 0) {
+//         return str.repeat(num);
+//     } else {
+//         return str.repeat(0);
+//     }
     // if num is greater than 0, repeat the string num times, else return an empty string
     return num > 0 ? str.repeat(num) : "";
 
