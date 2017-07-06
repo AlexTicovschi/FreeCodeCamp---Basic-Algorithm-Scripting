7828476328a6e18b76341a5a5a001455e1281fb3
Alex Ticovschi <alexmticovschi@gmail.com>
2017-07-06 20:53:26 +0100

    Update chunkArrayInGroups.js

--- chunkArrayInGroups.js
+++ chunkArrayInGroups.js
@@ -1,13 +1,12 @@
 
 function chunkArrayInGroups(arr, size) {
-
   var newArray = [];
   
   while(arr.length !== 0) {
     newArray.push(arr.splice(0, size));
   }
-  return newArray;
   
+  return newArray;
 }
 
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4);
