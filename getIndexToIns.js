b37860057ac54064744c13981f75974dd87e63c5
Alex Ticovschi <alexmticovschi@gmail.com>
2017-07-08 22:04:34 +0100

    Create getIndexToIns.js
    Returns the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.

---
+++ getIndexToIns.js
@@ -0,0 +1,16 @@
+// In this example, 10 - 35 = -25, which will increment the index to 1; 20 - 35 = 15, now the index equals 2; then 30 - 35 = -5 ...
+// increment the index again, index equals 3; 40 - 35 = 5, which does not satisfy the condition, so the num should be inserted at index 3 
+
+function getIndexToIns(arr, num) {
+  var index = 0;	
+  //loop through the array
+  for(var i = 0; i < arr.length; i++) {
+    // if the arrays value - number returns negative, increment the value of index by one
+    if(arr[i] - num < 0) {
+      index++;
+    }
+  }
+  return index;
+}
+
+getIndexToIns([10, 20, 30, 40, 50], 35);
