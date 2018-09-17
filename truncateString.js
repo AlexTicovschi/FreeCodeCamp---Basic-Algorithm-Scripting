9be72751e1c61bcbff04ec25eb177b6c3a948207
Alex Ticovschi <alexmticovschi@gmail.com>
2018-09-17 21:18:30 +0100

    Update truncateString.js

--- truncateString.js
+++ truncateString.js
@@ -1,11 +1,13 @@
 function truncateString(str, num) {
   
-  if(str.length > num && num <= 3) {
-    str = str.slice(0, num) + '...';
-  } else if (num <= 14) {
-    str = str.slice(0, (num - 3)) + '...';
-  } 
-  return str;
+//   if(str.length > num && num <= 3) {
+//     str = str.slice(0, num) + '...';
+//   } else if (num <= 14) {
+//     str = str.slice(0, (num - 3)) + '...';
+//   } 
+//   return str;
+  
+    return num < 12 ? str.substr(0, num) + '...' : str.substr(0, num);
 }
 
 truncateString("Peter Piper picked a peck of pickled peppers", 14);
