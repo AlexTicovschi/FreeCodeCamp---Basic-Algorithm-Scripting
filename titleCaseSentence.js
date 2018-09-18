87836be5828fb5c91d134a72cca016b8e75ed647
Alex Ticovschi <alexmticovschi@gmail.com>
2018-09-18 19:54:25 +0100

    Update titleCaseSentence.js

--- titleCaseSentence.js
+++ titleCaseSentence.js
@@ -6,19 +6,22 @@
 
 
 function titleCase(str) {
-  // convert the string to lower case and array of words and store it in 'words' variable
-  var words = str.toLowerCase().split(" ");
   
-  // loop through the words array
-  for(var i = 0; i < words.length; i++) {
-    // convert the array of words into an array of letters
-    var letters = words[i].split("");
-    // convert the first letter of each word to upper case
-    letters[0] = letters[0].toUpperCase(); 
-    words[i] = letters.join('');
-  }
-  // return the string
-  return words.join(' ');
+  return str.toLowerCase().split(' ').map(w => w[0].toUpperCase() + w.substr(1)).join(' ');
+  
+//   // convert the string to lower case and array of words and store it in 'words' variable
+//   var words = str.toLowerCase().split(" ");
+  
+//   // loop through the words array
+//   for(var i = 0; i < words.length; i++) {
+//     // convert the array of words into an array of letters
+//     var letters = words[i].split("");
+//     // convert the first letter of each word to upper case
+//     letters[0] = letters[0].toUpperCase(); 
+//     words[i] = letters.join('');
+//   }
+//   // return the string
+//   return words.join(' ');
 }
 
 titleCase("I'm a little tea pot");
