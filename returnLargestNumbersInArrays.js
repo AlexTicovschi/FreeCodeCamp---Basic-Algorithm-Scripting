c75dc5d844f995605d2a61fe69ce90786354a919
Alex Ticovschi <alexmticovschi@gmail.com>
2019-07-23 15:55:53 +0100

    third solution with for of loop and Math.max()

--- returnLargestNumbersInArrays.js
+++ returnLargestNumbersInArrays.js
@@ -1,22 +1,45 @@
-function largestOfFour(arr) {
-  
-//   let newArray = [];
-//   let sortedArr = [];
+/*** Return Largest Numbers in Arrays ***/
 
-//   for(let i = 0; i < arr.length; i++) {
-//     sortedArr.push(arr[i].sort((a,b) => b - a));
-//   }
-//   for(let i = 0; i < sortedArr.length; i++) {
-//     newArray.push(arr[i][0]);
-//   }
-//   return newArray;
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, 
+// the provided array will contain exactly 4 sub-arrays.
+
+// Remember, you can iterate through an array with a simple for loop, and access each member 
+// with array syntax arr[i].
+
+function largestOfFour(arr) {
+  /*** First Solution ***/
+  // let newArray = [];
+  // let sortedArr = [];
+
+  // for(let i = 0; i < arr.length; i++) {
+  //   sortedArr.push(arr[i].sort((a,b) => b - a));
+  // }
+  // for(let i = 0; i < sortedArr.length; i++) {
+  //   newArray.push(arr[i][0]);
+  // }
+  // return newArray;
   
+  
+  
+  /*** Second Solution ***/
   return arr.map(function(subArray) {
     //console.log(subArray);
     return subArray.reduce(function(num1, num2) {
       return (num1 > num2) ? num1 : num2;
     });
   });
+
+  
+  
+  /*** Third Solution ***/
+  
+	let output = [];
+
+	for (let list of arr) {
+		output.push(Math.max(...list));
+	}
+
+	return output;
   
 }
 
