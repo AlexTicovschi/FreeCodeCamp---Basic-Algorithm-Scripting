a57e74e7d399309c3b2e32ae672d18ebda9ac3b4
Alex Ticovschi <alexmticovschi@gmail.com>
2019-07-28 19:30:19 +0100

    second solution

--- getIndexToIns.js
+++ getIndexToIns.js
@@ -1,6 +1,7 @@
 // In this example, 10 - 35 = -25, which will increment the index to 1; 20 - 35 = 15, now the index equals 2; then 30 - 35 = -5 ...
 // increment the index again, index equals 3; 40 - 35 = 5, which does not satisfy the condition, so the num should be inserted at index 3 
 
+/*** First Solution ***/
 function getIndexToIns(arr, num) {
   var index = 0;	
   //loop through the array
@@ -11,6 +12,12 @@ function getIndexToIns(arr, num) {
     }
   }
   return index;
+}
+
+
+/*** Second Solution ***/
+function getIndexToIns(arr, num) {
+  return [...arr, num].sort((a,b) => a-b).indexOf(num);
 }
 
 getIndexToIns([10, 20, 30, 40, 50], 35);
