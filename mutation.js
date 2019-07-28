0766b7bba75f1454503823e334bbe2888fa569d4
Alex Ticovschi <alexmticovschi@gmail.com>
2019-07-28 19:56:17 +0100

    second solution

--- mutation.js
+++ mutation.js
@@ -1,3 +1,18 @@
+/********************************************/
+/*** Basic Algorithm Scripting: Mutations ***/
+/********************************************/
+
+// Return true if the string in the first element of the array contains all of the letters of the string
+// in the second element of the array.
+
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string 
+// are present in the first, ignoring case.
+
+// The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+// Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+
+
+/*** First Solution ***/
 function mutation(arr) {
   // turn every letter of the first string to lower case
   var str = arr[0].toLowerCase();
@@ -17,7 +32,21 @@ function mutation(arr) {
   
   //otherwise return true
   return true;
-  
+}
+
+
+/*** Second Solution ***/
+function mutation(arr) {
+  const word1 = arr[0].toLowerCase();
+  const word2 = arr[1].toLowerCase();
+
+  for(let val of word2) {
+    if(word1.indexOf(val) === -1) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 mutation(["hello", "hey"]);
